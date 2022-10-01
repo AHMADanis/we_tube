0d40c0205c09879b7fecdfa07996eced19ac8d1e
Anees Ahmad <inbox.a.ahmad@gmail.com>
2022-10-01 23:03:38 +0200

    commit

--- src/components/playback/PlayBack.js
+++ src/components/playback/PlayBack.js
@@ -1,21 +1,23 @@
 import React from "react";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 import "./PlayBack.css";
 
 
-function Playback() {
+function PlayBack() {
   return (
-      <div className='player-wrapper'>
-        <ReactPlayer
-          className='react-player'
-          url='https://www.youtube.com/watch?v=bEzbim8peGU'
-          width='100%'
-          height='100%'
-        />
-      </div>
+    <div className="player-wrapper">
+    <ReactPlayer
+      url="https://www.youtube.com/watch?v=bEzbim8peGU"
+      className="react-player"
+      playing
+      width="100%"
+      height="100%"
+      controls={true}
+    />
+  </div>
   );
 }
 
-export default Playback;
+export default PlayBack;
 
 
